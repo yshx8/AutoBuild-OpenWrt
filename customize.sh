45c7a1f922dce2805481f909da5b61af8097d0a8
yshx8 <giada22@qq.com>
2020-03-23 01:51:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/YSHX8/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/YSHX8/g' openwrt/package/lede/package/lean/autocore/files/index.htm
