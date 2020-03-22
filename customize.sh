d12233d2a01a8f3fd285dfca49392b6d6b6700c5
yshx8 <giada22@qq.com>
2020-03-23 02:46:45 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,4 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/YSHX8/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/YSHX8/g' openwrt/package/lede/package/lean/autocore/files/index.htm
+sed -i 's/<th class="cbi-section-table-cell"><%:MAC-Address%><\/th>/<th class="cbi-section-table-cell"><%:MAC-Address%><\/th>\n<th class="cbi-section-table-cell">Vendor<\/th>/g' openwrt/package/lean/autocore/files/index.htm
